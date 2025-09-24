25939fe40f2f80f347096371efb75564c46c2fdf
Ahmad Mayallo <ahmadmayallo02@gmail.com>
2025-09-24 16:01:46 +0300

    Add Hero Section in Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./route";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/custom/header";
+import Footer from "./components/custom/footer";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Categories from "./pages/categories";
+import Post from "./pages/post";
+import SignUp from "./pages/signUp";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+          <Route path="/contact" Component={Contact} />
+          <Route path="/categories" Component={Categories} />
+          <Route path="/post/:id" Component={Post} />
+          <Route path="/sign-up" Component={SignUp} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/home/hero.tsx
@@ -0,0 +1,56 @@
+import { Box, Container, Heading, Icon, Link, Text } from "@chakra-ui/react";
+import hero from "../../assets/images/hero-bg.jpg";
+import { FaArrowRight } from "react-icons/fa";
+
+export default function Hero() {
+  return (
+    <>
+      <Box as="section" className="hero center-y" bgImage={`url(${hero})`}>
+        <Container>
+          <Text
+            p="8px 16px"
+            cursor="initial"
+            className="main-button"
+            mx="auto"
+            w="fit"
+          >
+            Featured Article
+          </Text>
+          <Heading
+            mt={6}
+            color="#fff"
+            textAlign="center"
+            fontSize={36}
+            lineHeight={1.25}
+            fontWeight={700}
+          >
+            The Future of Web Development: Trends to Watch in 2025
+          </Heading>
+          <Text
+            textAlign="center"
+            my={4}
+            color="#fff"
+            fontSize={18}
+            lineHeight={2}
+          >
+            Discover the latest innovations and emerging technologies that will
+            shape the web development landscape in the coming year. From AI
+            integration to new frameworks.
+          </Text>
+          <Link
+            href="/categories"
+            p="8px 16px"
+            className="center-y main-button"
+            mx="auto"
+            w="fit"
+          >
+            Read Full Article
+            <Icon ms={2}>
+              <FaArrowRight />
+            </Icon>
+          </Link>
+        </Container>
+      </Box>
+    </>
+  );
+}
